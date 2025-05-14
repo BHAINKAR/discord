7c7e9201eff9a3ea1fd3b567e91f00929199b1e0
BHAINKAR <anishlado9@gmail.com>
2025-05-15 04:59:28 +0545

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Listening to ArctixMC", "🎮 Playing ArctixMC"];
+const statusMessages = ["Watching play.arctixmc.fun", "Playing play.arctixmc.fun"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
